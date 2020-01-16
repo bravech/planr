258323d9e3cea6eac91fa85f1a018671d02f4352
Charlie Phillips <cgp246@gmail.com>
2020-01-16 12:36:29 -0500

    login stuff

--- main.js
+++ main.js
@@ -11,6 +11,12 @@ app.get('/', (req, res) => {
     res.render('pages/index');
 });
 
+app.post('/storeauth', (req, res) => {
+    var code_secret = req.body
+    console.log(req.body)
+    res.send("asdf")
+})
+
 app.listen(5656, () => {
     console.log('http://localhost:5656')
 })
